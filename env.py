e97c7cfc6bb28c53fdab1d08bbb3b2cc3885618b
MrScabbyCreature <abhorgupta@gmail.com>
2020-08-08 19:49:58 +0530

    Added functionality to start from a specific state

--- env.py
+++ env.py
@@ -7,7 +7,7 @@ class CubeEnv(gym.Env):
 	''' Rubics cube gym environment '''
 	metadata = {'render.modes': ['term', 'gui']}
 
-	def __init__(self, n):
+	def __init__(self, n):		
 		super().__init__()
 		self.n = n
 		self.action_space = spaces.Tuple(
@@ -17,26 +17,41 @@ class CubeEnv(gym.Env):
 										spaces.Discrete((self.n - 1) // 2), #slice distance from face
 									)
 									)
+		self.face_order_for_obs = ['F', 'L', 'B', 'R', 'U', 'D']
 		self.cube = None
 		self.reset()
 
 	def step(self, action):
 		# Execute one time step within the environment
 		self.cube.rotate(*action)
-		face_order_for_obs = ['F', 'L', 'B', 'R', 'U', 'D']
-		obs = np.stack([self.cube.faces[face][...] for face in face_order_for_obs], axis=2)
-		reward = 0 # FIXME
+		obs = np.stack([self.cube.faces[face][...] for face in self.face_order_for_obs], axis=2)
+		reward = 0 # TODO: To experiment
 		done = self.cube.is_complete()
 		info = {}
 		return obs, reward, done, info
 
-	def reset(self, randomize=True):
-		# Reset the state of the environment to an initial state
+	def reset(self, randomize=True, state=None):
+		'''Reset the env
+		args
+		----
+		randomize(optional): Bool
+			If True, returns a random state. If False, returns a completed cube. True by default
+		state(optiona): 3x3x6 np.array representing a valid cube state
+			If provided, then used as starting state. Overrides 'randomize'
+		'''
 		del self.cube
 		self.cube = Cube(self.n)
-		if randomize:
+		if randomize and isinstance(state, type(None)):
 			for i in range(self.n * 10):
 				self.step(self.action_space.sample())
+		
+		if state is not None:
+			assert state.shape == (3, 3, 6), f"{state.shape} is incorrect shape."
+			for i in range(6):
+				self.cube.faces[self.face_order_for_obs[i]][...] = state[:, :, i]
+				
+		obs = np.stack([self.cube.faces[face][...] for face in self.face_order_for_obs], axis=2)
+		return obs
 
 	def render(self, mode='gui'):
 		# Render the environment to the screen
